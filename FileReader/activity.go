71c829cf6b45286f87ea986ef970ded408d617d2
ganitagya <akash.mhptr@gmail.com>
2018-03-07 19:32:21 +0530

    check

---
+++ FileReader/activity.go
@@ -0,0 +1,40 @@
+package MQTT_Pub
+
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+// log is the default package logger
+var log = logger.GetLogger("activity-akash file reader")
+
+const (
+	filename   = "filename"
+)
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new AppActivity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	// do eval
+
+	
+
+	return true, nil
+}
+
